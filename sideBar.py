6c57ed3774309973ce14297327aa4822a7ab61cb
“lencshu <lencshu@gmail.com>
2017-11-19 14:37:34 +0100

    Webpage

---
+++ sideBar.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*- 
+from bs4 import BeautifulSoup
+import re
+import os
+
+if os.path.exists("index.html") :
+	os.remove("index.html")
+html = open('DayPo.html',"r+")
+
+soup = BeautifulSoup(html, "html.parser")
+#Rename
+divto = soup.find("div", class_="to")
+divtoc = soup.find("div", class_="toc")
+print divto
+if divto!=None or divto!="": 
+    divto.append(divtoc)
+divto.div['class'] = 'tod'
+html = soup.prettify("utf-8")
+
+keyword="outline: 1300px solid #fff;"
+post = html.find(keyword)
+if post != -1:
+    html = html[:post+len(keyword)]+"float:right;padding-left:10px;width:60%;"+html[post+len(keyword):]
+	#html = html.replace('///C://Users/lencs/Desktop/8.智能硬件/git.IOT/', '')
+    #html = html.replace('C:\\Users\\lencs\\Desktop\\8.智能硬件\\git.IOT\\', '')
+    file = open('index.html', 'w')
+    file.write(html)
+file.close( )
+os.remove("DayPo.html")
